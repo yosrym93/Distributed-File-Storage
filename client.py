2649c1f2deac6d197449debfcb9b5cd8a3008ab0
mahmoud sebak <mahmoud.sebak1998@gmail.com>
2020-03-07 16:08:59 +0200

    Client and Master ports

---
+++ client.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import zmq
+import sys
+import time
+import cv2
+import pickle
+
+def master_connection(context, master_link):
+    master_socket = context.socket(zmq.REQ)
+    master_socket.connect ("tcp://{link}".format(link = master_link))
+    socket.send_pyobj((file_name, UpDown))
+    #recieve port from master
+    datakeeper_link = socket.recv()
+    return datakeeper_link
+
+def datakeeper_connection(context, datakeeper_link):
+    datakeeper_socket = context.socket(zmq.PAIR)
+    datakeeper_socket.connect ("tcp://{link}".format(link = datakeeper_link))
+    return datakeeper_socket
+    
+def upload(datakeeper_socket):
+    with open(file_name, "rb") as video_file:
+        video = video_file.read()
+    msg = datakeeper_socket.send(pickle.dumps(video))
+    return
+    
+def download(datakeeper_socket):
+    video = pickle.loads(datakeeper_socket.recv())
+    with open(file_name, "wb") as video_file:
+        video_file.write(video)
+    return
+    
+def main():
+    _, master_link, file_name, UpDown = sys.argv
+    context = zmq.Context()
+    datakeeper_link = master_connection(context, master_link)
+    datakeeper_socket = datakeeper_connection(context, datakeeper_link)
+
+    #send request to data keeper
+    datakeeper_socket.send(pickle.dumps((file_name,UpDown)))
+    if(UpDown == "0"):
+        upload(datakeeper_socket)
+    else:
+        download(datakeeper_socket)
+    time.sleep(1)
+    datakeeper_socket.close()
+    
+if __name__ == '__main__':
+    main()
