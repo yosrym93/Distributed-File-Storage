12d1e201bd743787bce138bd04d9f772b2452ee7
Yosry Ahmed <yosrym93@gmail.com>
2020-03-08 20:25:55 +0200

    Update data keepers run script

--- data_keeper/run.py
+++ data_keeper/run.py
@@ -21,25 +21,26 @@ data_keepers_ips = [
     '127.0.0.1'
 ]
 
-# Create heartbeat process
-subprocess.Popen(['python', 'heartbeat.py', id, master_ip, master_heartbeat_port])
+if __name__ == '__main__':
+	# Create heartbeat process
+	subprocess.Popen(['python', 'heartbeat.py', id, master_ip, master_heartbeat_port])
 
-# Create replicate process
-subprocess.Popen(['python', 'replicate.py', id, master_ip, master_replicate_port, replicate_port, data_keepers_count,
-                  *data_keepers_ips])
+	# Create replicate process
+	subprocess.Popen(['python', 'replicate.py', id, master_ip, master_replicate_port, replicate_port, data_keepers_count,
+		          *data_keepers_ips])
 
-# Delete videos directory if it exists
-try:
-    shutil.rmtree(videos_dir)
-except OSError as e:
-    pass
+	# Delete videos directory if it exists
+	try:
+	    shutil.rmtree(videos_dir)
+	except OSError as e:
+	    pass
 
-# Create new videos directory
-os.mkdir(videos_dir)
+	# Create new videos directory
+	os.mkdir(videos_dir)
 
-# Create file transfer processes
-for port in range(file_transfer_ports_start, file_transfer_ports_start + file_transfer_ports_count):
-    subprocess.Popen(['python', 'file_transfer.py', id, master_ip, master_file_transfer_port, str(port), videos_dir])
+	# Create file transfer processes
+	for port in range(file_transfer_ports_start, file_transfer_ports_start + file_transfer_ports_count):
+	    subprocess.Popen(['python', 'file_transfer.py', id, master_ip, master_file_transfer_port, str(port), videos_dir])
 
-input('Press any key to exit..\n')
-os.killpg(0, signal.SIGKILL)
+	input('Press any key to exit..\n')
+	os.killpg(0, signal.SIGKILL)
