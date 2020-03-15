c25db2603275f41a2ca7b40f60d0b66b5c3570ff
mahmoudsebak <mahmoudsebak1998@gmail.com>
2020-03-15 16:48:40 +0200

    Connecting client to all master ports

--- client.py
+++ client.py
@@ -3,60 +3,61 @@ import sys
 import pickle
 import random
 
-def master_connection(context, master_link, file_name, UpDown):
+
+def master_connection(context, master_ip, file_name, UpDown, port_list):
     master_socket = context.socket(zmq.REQ)
-    master_socket.connect ("tcp://{link}".format(link = master_link))
+    for port in port_list:
+        print(port)
+        master_link = master_ip + ":" + str(port)
+        master_socket.connect("tcp://{link}".format(link=master_link))
     master_socket.send_pyobj((file_name, UpDown))
     print("Request sent to master datahandler")
-    print(" This request is through link %s",master_link)
-    #recieve port from master
+    # recieve port from master
     datakeeper_link = master_socket.recv_string()
     print("datakeeper link: " + datakeeper_link)
     return datakeeper_link
 
+
 def datakeeper_connection(context, datakeeper_link):
     datakeeper_socket = context.socket(zmq.PAIR)
-    datakeeper_socket.connect ("tcp://{link}".format(link = datakeeper_link))
+    datakeeper_socket.connect("tcp://{link}".format(link=datakeeper_link))
     return datakeeper_socket
-    
+
+
 def upload(datakeeper_socket, file_name):
     with open(file_name, "rb") as video_file:
         video = video_file.read()
     msg = datakeeper_socket.send(pickle.dumps(video))
     return
-    
+
+
 def download(datakeeper_socket, file_name):
     video = pickle.loads(datakeeper_socket.recv())
     with open(file_name, "wb") as video_file:
         video_file.write(video)
     return
-    
+
+
 def main():
     _, master_ip, master_port, ports_count, file_name, UpDown = sys.argv
     context = zmq.Context()
-    checked_ports_list=[]
-    get_port_flag=False
-    print(ports_count)
-    while len(checked_ports_list)!=(int(ports_count)):
-        current_port=random.randint(int(master_port),int(master_port) + int(ports_count)-1)
-        if current_port not in checked_ports_list:
-            checked_ports_list.append(current_port)
-            master_link = master_ip + ":" + str(current_port)
-            datakeeper_link = master_connection(context, master_link, file_name, UpDown)
-            if  datakeeper_link:
-                get_port_flag=True 
-                break
-    if not get_port_flag:
+    # connect to all master ports randomly
+    port_list = list(range(int(master_port), int(master_port) + int(ports_count)))
+    random.shuffle(port_list)
+    datakeeper_link = master_connection(context, master_ip, file_name, UpDown, port_list)
+
+    if not datakeeper_link:
         print('No empty ports on the server')
         return
     datakeeper_socket = datakeeper_connection(context, datakeeper_link)
-    #send request to data keeper
+    # send request to data keeper
     datakeeper_socket.send(pickle.dumps((file_name, UpDown)))
-    if(UpDown == "0"):
+    if (UpDown == "0"):
         upload(datakeeper_socket, file_name)
     else:
         download(datakeeper_socket, file_name)
-    datakeeper_socket.disconnect("tcp://{link}".format(link = datakeeper_link))
-    
+    datakeeper_socket.disconnect("tcp://{link}".format(link=datakeeper_link))
+
+
 if __name__ == '__main__':
     main()
